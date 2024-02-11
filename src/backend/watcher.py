d3a0833a6312740365746915f90c562c79e48704
Vishnu Nair <76607707+nairvishnumail@users.noreply.github.com>
2024-02-11 17:43:50 -0500

    Update watcher.py

--- src/backend/watcher.py
+++ src/backend/watcher.py
@@ -1,25 +1,25 @@
-import ai.AIPipeline as processor
 import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 #As long as main is running, the code in this file will run the processor whenever an mp4 is placed into the input directory
-input_directory_path = 'inputs'
+input_directory_path = 'C:/Users/High Definition/SharprAI/src/backend/ai/input'
+# C:/Users/High Definition/SharprAI/src/backend/ai/input
+# /workspace/tensorrt/input/
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
-        if not event.is_directory and event.src_path.endswith(('.mp4')): 
-            print(f"------ processor triggered: {os.path.basename(event.src_path)} added to watched directory '{input_directory_path}' : ")
-            processor.run()
-            print(f"------ processor terminated: {os.path.basename(event.src_path)} processed from watched directory '{input_directory_path}'.\n")
+        if not event.is_directory:
+            file_extension = os.path.splitext(event.src_path)[1]
+            if file_extension.lower() == '.mp4' or file_extension.lower() == '.mkv':
+                print("Process triggered: ", os.path.basename(event.src_path))
 
 if __name__ == "__main__":
-    
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, input_directory_path, recursive=False)
-    print(f"\n--------------------\nwatcher started: watching directory '{input_directory_path}'\n--------------------\n")
+    print("Watcher started: Monitoring directory", input_directory_path)
     observer.start()
 
     try:
@@ -29,6 +29,3 @@ if __name__ == "__main__":
         observer.stop()
 
     observer.join()
-
-
-
