025c705002d2b61d3ca8ab1271e8842c16fbef37
PeterFavero <peterfavero10@gmail.com>
2024-02-11 17:52:23 -0500

    Add subprocess command and improve documentation

--- src/backend/watcher.py
+++ src/backend/watcher.py
@@ -2,6 +2,7 @@ import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import subprocess
 
 #As long as main is running, the code in this file will run the processor whenever an mp4 is placed into the input directory
 input_directory_path = 'C:/Users/High Definition/SharprAI/src/backend/ai/input'
@@ -13,13 +14,16 @@ class MyHandler(FileSystemEventHandler):
         if not event.is_directory:
             file_extension = os.path.splitext(event.src_path)[1]
             if file_extension.lower() == '.mp4' or file_extension.lower() == '.mkv':
-                print("Process triggered: ", os.path.basename(event.src_path))
+                print("     -- watcher.py triggered: beginning processing", os.path.basename(event.src_path))
+                subprocess.run(["docker-compose", "run", "--rm", "vsgan_tensorrt"])
+                subprocess.run(["python", "main.py"])
+                print("     -- watcher.py terminated: finished processing", os.path.basename(event.src_path))
 
 if __name__ == "__main__":
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, input_directory_path, recursive=False)
-    print("Watcher started: Monitoring directory", input_directory_path)
+    print(" ------------ \n Watcher started: Monitoring directory", input_directory_path, " \n ------------ \n ")
     observer.start()
 
     try:
