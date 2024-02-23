9936496764f6d3fe3f6031c652b872f5b904a6f4
Peter Favero <peterfavero10@gmail.com>
2024-02-23 12:43:19 -0500

    Delete src directory in organized: making refactoring changes

--- src/screen_recorder/README.md
+++
@@ -1,3 +0,0 @@
-# screen-rec
-
-full readme coming soon - TLDR feel free to submit a PR.
--- src/youtube_url/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "startRecording") {
-      console.log("Start recording the tab");
-      // Implement recording functionality here
-    }
-  });
-  
