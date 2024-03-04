d5f1c6dd434a46ba961814ddd1fa11197fb8bf54
Vishnu Nair <76607707+nairvishnumail@users.noreply.github.com>
2024-03-04 13:16:22 -0500

    Delete src directory

--- src/backend/ai/inference.py
+++
@@ -1,9 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore")
-import sys
-sys.path.append("/workspace/tensorrt/")
-from inference_config import inference_clip
-
-video_path = ""
-clip = inference_clip(video_path)
-clip.set_output()
